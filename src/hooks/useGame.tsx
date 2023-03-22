00be7dfcda0b9e683d30a27bca4a4a12fa274f95
LucasLvy <lucaslevy10@gmail.com>
2023-03-22 11:51:23 +0100

    feat(front): monkey connection :monkey:

--- src/hooks/useGame.tsx
+++ src/hooks/useGame.tsx
@@ -2,6 +2,15 @@ import { useCallback, useEffect, useState } from 'react'
 import { getCellPosition, getCells, Grid, randomizeGrid, transformGrid } from '../utils'
 import { COLUMNS, INTERVAL } from '../utils/constants'
 import useInterval from 'use-interval'
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { ContractPromise } from "@polkadot/api-contract";
+
+import { Keyring } from "@polkadot/keyring";
+import util_crypto from "@polkadot/util-crypto";
+import type { WeightV2 } from '@polkadot/types/interfaces'
+import { BN, BN_ONE } from "@polkadot/util";
+const MAX_CALL_WEIGHT = new BN(5_000_000_000_000).isub(BN_ONE);
+const PROOFSIZE = new BN(1_000_000);
 
 const useGame = () => {
   const [isRunning, setIsRunning] = useState<boolean>(false)
@@ -37,9 +46,34 @@ const useGame = () => {
   }, [])
 
   const handleNext = useCallback(() => {
-    const nextGrid = transformGrid(grid)
-    setGrid(nextGrid)
-    setSteps(steps + 1)
+    const wsProvider = new WsProvider('wss://0adf-2a0d-3344-18-3b10-b849-40b2-1e09-ad33.eu.ngrok.io');
+    const api = ApiPromise.create({
+      provider: wsProvider,
+    }).then(api => {
+      const metadata = require("./mikasa.json");
+      const address = "5HPhYELrkS96S7bG8wcTUdji869XJmc7za6MVd9ks5nse9so";
+      const contract = new ContractPromise(api, metadata, address);
+
+      const keyring = new Keyring({ type: 'sr25519' });
+
+      const alicePair = keyring.addFromUri('//Alice', { name: 'Alice default' });
+
+      // (We perform the send from an account, here using Alice's address)
+      contract.query.get(
+        alicePair.address,
+        {
+          gasLimit: api?.registry.createType('WeightV2', {
+            refTime: MAX_CALL_WEIGHT,
+            proofSize: PROOFSIZE,
+          }) as WeightV2,
+          storageDepositLimit: undefined,
+        }
+      ).then(obj => {
+        // @ts-ignore: Object is possibly 'null'.
+        setGrid(obj.output?.toJSON()["ok"])
+      });
+
+    });
   }, [grid, steps])
 
   const handleCell = useCallback((column: number, rowIndex: number, cell: boolean) => {
